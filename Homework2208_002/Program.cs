25f9fd6be4cfb49129c8ab06d9a2197c1609355b
Сергей Валерьевич Матвеев <110776836+1sergei2@users.noreply.github.com>
2022-08-25 07:37:30 +0500

    задача 13

---
+++ Homework2208_002/Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintThirdNumber()
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    string x1 = Convert.ToString(x);
+    if (x1.Length > 2)
+    {
+        Console.WriteLine("Третья цифра " + x1[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+PrintThirdNumber();
