573e4af63caff5c849bb51de06c5670ad0580288
Сергей Валерьевич Матвеев <110776836+1sergei2@users.noreply.github.com>
2022-08-25 11:26:09 +0500

    задача 15

---
+++ Homework2208_003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите цифру, согласно дню недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+void WhatDayIsIt(int x)
+{
+    if (x == 6 || x == 7)
+    {
+        Console.WriteLine("Это выходной день");
+    }
+    else if (x < 1 || x > 7)
+    {
+        Console.WriteLine("Это не день недели, балбес");
+    }
+    else Console.WriteLine("Это будний день");
+}
+WhatDayIsIt(x);
