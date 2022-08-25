d01fa4b40f560dc306c98c435d149ddea3a1f26c
Сергей Валерьевич Матвеев <110776836+1sergei2@users.noreply.github.com>
2022-08-25 12:58:23 +0500

    До перерыва

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -65,46 +65,98 @@
 
 
 
-    // Console.WriteLine("Введите 2 числа ");
-    // int x = Convert.ToInt32(Console.ReadLine());
-    // int y = Convert.ToInt32(Console.ReadLine());
-    // if (x > y)
-    // {
-    //     if (x % y == 0) Console.WriteLine("кратно ");
-    //     else Console.WriteLine("не кратно " + x % y);
-    // }
-    // else
-    // {
-    //     if (y % x == 0) Console.WriteLine("Кратно ");
-    //     else Console.WriteLine("Не кратно " + y % x);
-    // }
+// Console.WriteLine("Введите 2 числа ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (x > y)
+// {
+//     if (x % y == 0) Console.WriteLine("кратно ");
+//     else Console.WriteLine("не кратно " + x % y);
+// }
+// else
+// {
+//     if (y % x == 0) Console.WriteLine("Кратно ");
+//     else Console.WriteLine("Не кратно " + y % x);
+// }
 
-    Console.WriteLine("Введите первое целое число: ");
-    int number1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите второе целое число: ");
-    int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое целое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе целое число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-    string Multiplicity(int num1, int num2)
-    {
-        string multiplicity = String.Empty;
-        int max = num1;
-        int min = num2;
-        if (num2 > num1)
-        {
-            max = num2;
-            min = num1;
-        }
+// string Multiplicity(int num1, int num2)
+// {
+//     string multiplicity = String.Empty;
+//     int max = num1;
+//     int min = num2;
+//     if (num2 > num1)
+//     {
+//         max = num2;
+//         min = num1;
+//     }
 
-        if (max % min == 0)
-        {
-            multiplicity = $"Число {max} кратно {min}";
-        }
-        else
-        {
-            multiplicity = $"Число {max} не кратно {min}, остаток {max % min}";
-        }
+//     if (max % min == 0)
+//     {
+//         multiplicity = $"Число {max} кратно {min}";
+//     }
+//     else
+//     {
+//         multiplicity = $"Число {max} не кратно {min}, остаток {max % min}";
+//     }
 
-        return multiplicity;
-        }
-    Console.WriteLine(Multiplicity(number1, number2));
+//     return multiplicity;
+//     }
+// Console.WriteLine(Multiplicity(number1, number2));
+
+// int GetNum4()
+//         {
+//             Console.Write("Введите координату х ");
+//             int x = int.Parse(Console.ReadLine());
+//             Console.Write("Введите координату y ");
+//             int y = int.Parse(Console.ReadLine());  
+//             int res;
+//             if ( x>0 && y>0 )   res=1;
+//             else if ( x<0 && y>0 )   res=2;
+//             else if ( x<0 && y<0 )   res=3;
+//             else if ( x>0 && y<0 )   res=4; 
+//             else res=0; 
+//             return res;
+//         }
+
+//         try
+//             {
+//             Console.WriteLine(GetNum4());
+//             }
+//         catch
+//             {
+//             Console.WriteLine("Надо было вводить именно числа, и именно целые!");    
+//             }
+
+
+
+// Задача 18: Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// string GetNum4()
+// {
+//     Console.Write("Введите число от 1 до 4: ");
+//     int res = Convert.ToInt32(Console.ReadLine());
+//     string what = string.Empty;
+
+//     if (res == 1) what = "x > 0, y > 0";
+//     else if (res == 2) what = "x < 0, y > 0";
+//     else if (res == 3) what = "x < 0, y < 0";
+//     else if (res == 4) what = "x > 0, y < 0";
+//     else Console.WriteLine("Надо было вводить числа от 1 до 4!");
+//     return what;
+// }
+
+// try
+//     {
+//     Console.WriteLine(GetNum4());
+//     }
+// catch
+//     {
+//     Console.WriteLine("Надо было вводить цифры");
+//     }
 
