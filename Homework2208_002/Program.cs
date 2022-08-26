d867fabdbbc446eb1891191334121afdc9a65c92
Сергей Валерьевич Матвеев <110776836+1sergei2@users.noreply.github.com>
2022-08-26 06:06:23 +0500

    Поделились способом решения задачи

--- Homework2208_002/Program.cs
+++ Homework2208_002/Program.cs
@@ -13,3 +13,23 @@
     }
 }
 PrintThirdNumber();
+
+// Программа, которая выводит третью цифру заданного числа
+// или сообщает, что такого числа нет.
+
+// void PrintThirdnum()
+// {
+// int a = 0;
+// Console.WriteLine("Ведите целое число");
+// int x = Convert.ToInt32(Console.ReadLine());
+// if (x < -99) {a = -1*x;} else {a = x;}
+// if (a > 99) 
+//     { 
+//         while (a > 999) {a = a/10;}
+//         Console.WriteLine("Третья цифра в числе " + x + "  равна " + a%10);
+//     }
+// else {Console.WriteLine("Нет третьей цифры слева. Введенное число НЕ является трехзначным");}
+
+// return;
+// }
+// PrintThirdnum();
