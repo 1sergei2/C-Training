96e4637cfaa2d8725416068a8febfbac077b8507
Сергей Валерьевич Матвеев <110776836+1sergei2@users.noreply.github.com>
2022-09-19 06:39:07 +0500

    дз от 16 сентября

---
+++ Homework_1609/Program.cs
@@ -0,0 +1,81 @@
+﻿//  Задача 64: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8
+
+/*
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+NaturalEvenNumber(m, n);
+
+int NaturalEvenNumber(int m, int n)
+{
+    if (m == n)
+    {
+         Console.Write($"{n} ");
+         return n;
+    }
+    else if (m % 2 == 0)
+    {
+        Console.Write($"{m} ");
+        m++;
+    }
+    else m++;
+    return NaturalEvenNumber(m, n);
+}
+*/
+
+/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+/*
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+*/
+
+// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+
+// M = 28; N = 7 -> 7
+/*
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+GreatestCommonDivisor(m, n);
+Console.WriteLine(GreatestCommonDivisor(m, n));
+int GreatestCommonDivisor(int m, int n)
+{
+     if (n == 0)
+        return m;
+    else
+        return GreatestCommonDivisor(n, m % n);
+}
+*/
